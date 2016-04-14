f13b147e031641f7376326ed9ae9d286753ac83a
HemzLaptop <Konrad622@gmail.com>
2016-04-14 02:39:48 +0200

    simple: product list, and product page

--- src/AppBundle/Controller/searchMainCategoriesController.php
+++ src/AppBundle/Controller/searchMainCategoriesController.php
@@ -139,6 +139,58 @@ class searchMainCategoriesController extends Controller
         return new JsonResponse(array('mainCategories' =>$mainCategoriesArray));
 
     }
+    /**
+     * @Route("/getProductsByCategory/{id}", name="getProductsByCategory")
+     * @Method("GET")
+     */
+    public function getProductsByCategory($id){
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Product');
+        $qb= $repository->createQueryBuilder('c');
+        $qb->select('c')
+            ->where('c.category =:categoryid')
+            ->setParameter('categoryid', $id);
+        $query = $qb->getQuery();
+        $products= $query -> getResult();
+        $productsArray = array();
+        $i = 0;
 
+        foreach($products as $product){
+            $productsArray[$i]['id'] = $product->getId();
+            $productsArray[$i]['nazwa'] = $product->getName();
+            $productsArray[$i]['cena'] = $product->getPrice();
+            $productsArray[$i]['dostepnosc'] = $product->getAmount();
+
+
+            $i++;
+        }
+
+        return new JsonResponse(array('products' =>$productsArray));
+    }
+
+    /**
+     * @Route("/search/getProductById/{id}", name="getProductById")
+     * @Method("GET")
+     */
+    public function getProductById($id){
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Product');
+        $qb= $repository->createQueryBuilder('c');
+        $qb->select('c')
+            ->where('c.id =:productid')
+            ->setParameter('productid', $id);
+        $query = $qb->getQuery();
+        $product= $query -> getResult();
+        $productArray = array();
+
+        $productArray[0]['id'] = $product->getId();
+        $productArray[0]['nazwa'] = $product->getName();
+        $productArray[0]['cena'] = $product->getPrice();
+        $productArray[0]['dostepnosc'] = $product->getAmount();
+        $productArray[0]['opis'] = $product->getDescription();
+        $productArray[0]['picture_path'] = $product->getPicturePath();
+
+
+
+        return new JsonResponse(array('product' =>$productArray));
+    }
 
 }
